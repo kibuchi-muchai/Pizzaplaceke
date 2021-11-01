2b064ab0a6495f5c7bf88cf0684fdb81c23914bb
kibuchi muchai <me@kibuchimuchai>
2021-11-01 11:34:00 +0300

    added business logic to js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 var username = prompt("whats your name?")
 document.write(`Hello${username},how are you? please make your orders and we will deliver within 24hrs`)
+
+//business logic
+function Order(size, crust,toppings,quantity,delivery) {
+    this.pizzaSize = size;
+    this.crust = crust;
+    this.pizzatoppings = toppings;
+    this.pizzaquantity = quantity;
+    this.pizzadelivery = delivery;
+}
+Order.prototype.allInformation=function(){
+    return this.sizeofpizza + "," + this.crust + "," + this.pizzatoppings + "," + this.pizzaquantity + ","}
