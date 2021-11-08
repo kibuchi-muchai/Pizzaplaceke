15208ee1d7ead910d9d52f36044b33502180335f
kibuchi muchai <me@kibuchimuchai>
2021-11-08 19:13:13 +0300

    added a function amd value for the pizza chosen

--- script.js
+++ script.js
@@ -9,76 +9,40 @@ function Order(size, crust,toppings,quantity,delivery) {
     this.pizzaquantity = quantity;
     this.pizzadelivery = delivery;
 }
-Order.prototype.allInformation=function(){
-    return this.sizeofpizza + "," + this.crust + "," + this.pizzatoppings + "," + this.pizzaquantity + "," + this.pizzadelivery}
-
-    //user interface logic
-$(document).ready (function(){
-    $("form#pizza").submit(function(event){
-        event.preventDefault();
-
-    var inputtedpizzasize=$("#size").val();
-    console.log(inputtedpizzasize)
-
-     var inputtedcrust=$("#crust").val();
-    console.log(inputtedcrust)
-
-    var thechoosenpizzatoppings=[];
-    $('input[type=checkbox]:checked').each(function() {
-        thechoosenpizzatoppings.push($(this).val());
-    }
-   ,); 
-    }
-,)})
-
-console.log(thechoosenpizzatoppings);
-var additiontoppings = 0;
-for(var a=0; a<thechoosenpizzatoppings.length; a++){
-    if(thechoosenpizzatoppings[a]==="Spicy pepperoni $ pepper"){
-        additiontoppings += 500
-        console.log(additiontoppings)
-    }
-    else if (thechoosenpizzatoppings[a]==="mushroom"){
-        additiontoppings +=500
-        console.log(additiontoppings)
-    }
-    else if (thechoosenpizzatoppings[a]==="extra cheese"){
-        additiontoppings +=700
-        console.log(additiontoppings)
-    }
-    else if (thechoosenpizzatoppings[a]==="green pepper"){
-        additiontoppings +=900
-        console.log(additiontoppings)
-    }
-}
-console.log(additiontoppings)
-var inputtedpizzaquantity
-var inputtedpizzadelivery =$('delivery').val()
-
-if($('#pizzaquantity').val()){
-    inputtedpizzaquantity=$('#delivery').val();
-    console.log(inputtedpizzaquantity)
-}
-if($('#pizzaquantity').val()){
-    console.log()
-
-
-var newOrder;
-var newOrder = new Order(inputtedpizzasize,inputtedcrust,additiontoppings,inputtedpizzaquantity,inputtedpizzadelivery);
-console.log(newOrder)
-console.log($('#delivery').val())
-$("#get-pizza").show();
-$(".pizza-size").text(newOrder.pizzaSize);
-$(".crust").text(newOrder.crust);
-
-$(".pizza-toppings").text(newOrder.pizzaSize);
-$(".pizza-quantity").text(newOrder.pizzaquantity);
-
-var yourlocation=prompt("please type in your location:");
-alert("your order has been received and we will deliver to" + yourlocation + ".Thank you for making your orders");
-}
-else {
-    $(".pizza-delivery").text("please select whether to deliver or not");
-}
-
-
+//user interface logic
+let firstPizza = new Pizza("small","spicy pepperoni & pepper","crispy",1000,1)
+let secondPizza = new Pizza("small","spicy pepperoni & pepper","stuffed",1000,2)
+let thirdPizza = new Pizza("small","spicy pepperoni & pepper","gluten-free",1000,3)
+let fourthPizza = new Pizza("medium","spicy pepperoni & pepper","crispy",1000,4)
+let fifthPizza = new Pizza("medium","spicy pepperoni & pepper","stuffed",1000,5)
+let sixthPizza = new Pizza("medium","spicy pepperoni & pepper","gluten-free",1000,6)
+let seventhPizza = new Pizza("large","spicy pepperoni & pepper","crispy",1000,7)
+let eighthPizza = new Pizza("large","spicy pepperoni & pepper","stuffed",1000,8)
+let ninethPizza = new Pizza("large","spicy pepperoni & pepper","gluten-free",1000,9)
+let tenthPizza = new Pizza("small","mushroom","crispy",1000,10)
+let eleventhPizza = new Pizza("small","mushroom","stuffed",1000,11)
+let twelvethPizza = new Pizza("small","mushroom","gluten-free",1000,12)
+let thirteenthPizza = new Pizza("medium","mushroom","crispy",1000,13)
+let fourteenthPizza = new Pizza("medium","mushroom","stuffed",1000,14)
+let fifteenththPizza = new Pizza("medium","mushroom","gluten-free",1000,15)
+let sixteenthPizza = new Pizza("large","mushroom","crispy",1000,16)
+let seventeenthPizza = new Pizza("large","mushroom","stuffed",1000,17)
+let eighteenththPizza = new Pizza("large","mushroom","gluten-free",1000,18)
+let nieteenthPizza = new Pizza("small","extracheese","crispy",1000,19)
+let twentiethPizza = new Pizza("small","extracheese","stuffed",1000,20)
+let twentyfirstPizza = new Pizza("small","extracheese","gluten-free",1000,21)
+let twentysecondPizza = new Pizza("medium","extracheese","crispy",1000,22)
+let twentythirdPizza = new Pizza("medium","extracheese","stuffed",1000,23)
+let twentyfourthPizza = new Pizza("medium","extracheese","gluten-free",1000,24)
+let twentyfifthPizza = new Pizza("large","extracheese","crispy",1000,25)
+let twentysixththPizza = new Pizza("large","extracheese","stuffed",1000,26)
+let twentysevenththPizza = new Pizza("large","extracheese","gluten-free",1000,27)
+let twentyeighthPizza = new Pizza("small","green pepper","crispy",1000,28)
+let twentyninethPizza = new Pizza("smal","green pepper","stuffed",1000,29)
+let thirtiethPizza = new Pizza("small","green pepper","gluten-free",1000,30)
+let thirtyfirstPizza = new Pizza("medium","green pepper","crispy",1000,31)
+let thirtysecondPizza = new Pizza("medium","green pepper","stuffed",1000,32)
+let thirtythirdPizza = new Pizza("medium","green pepper","gluten-free",1000,33)
+let thirtyfourthfPizza = new Pizza("large","green pepper","crispy",1000,34)
+let thirtyfifthPizza = new Pizza("large","green pepper","stuffed",1000,35)
+let thirtysixthPizza = new Pizza("large","green pepper","gluten-free",1000,36)
